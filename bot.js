61b4d4288e87a354f4a896ce736f4a13d2b5386a
Joao Bragança <jvbragancaa@gmail.com>
2020-10-11 00:04:09 -0300

    Created .env file for Heroku implementation

--- bot.js
+++ bot.js
@@ -1,6 +1,11 @@
-const config = require("./config/config");
+require("dotenv").config();
 const twit = require("twit");
-const T = twit(config);
+const T = twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
 
 console.log(`Bot started - ${new Date()}`);
 
